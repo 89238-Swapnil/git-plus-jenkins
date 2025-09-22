4c061f63d884c76af593dffcb93492f9c37d11d8
swapnil tiwade <swapniltiwade78@gmail.com>
2025-09-22 20:37:22 +0530

    Add HelloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+public class HelloWorld {
+    public static void main(String[] args) {
+        System. out.println("Hello, World!");
+    }
+}
+=======
+package pr;
+
+public class helloWorld {
+	
+	    public static void main(String[] args) {
+	        System. out.println("Hello, World!");
+	    }
+	}
+
+
+>>>>>>> d81ed13 (added)
